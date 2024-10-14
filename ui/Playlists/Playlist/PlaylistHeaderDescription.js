62089fb5cf3702bd550d4663f837eacde5793773
Evgeny <153275378+cosackHector@users.noreply.github.com>
2024-10-14 18:42:47 +0300

    ads

--- ui/Playlists/Playlist/PlaylistHeaderDescription.js
+++ ui/Playlists/Playlist/PlaylistHeaderDescription.js
@@ -5,6 +5,8 @@ export const PlaylistHeaderDescription = (titleText, arrayTracks) => {
     const element = createElement('div', ['container_headerPlaylist_description']);
     const titleElement = createElement("h2", ["playlist_header__title"]);
     const topArtist = getTopsArtist(arrayTracks);
+    const othersText = createElement('span', ['others_text'], {}, 'and others')
+    const others = document.querySelector('.others_text');
     const text = `${topArtist} and others`;
     const topArtistText = createElement('p', ['header_description__top'], {}, text);
 
